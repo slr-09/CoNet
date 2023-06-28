bb733e8d70a638ff799443a667210ace3801bf2f
slr-09 <ge12090@naver.com>
2023-06-28 23:48:51 +0900

    Feat: 애플 로그인 기능 구현

--- CoNet/SceneDelegate.swift
+++ CoNet/SceneDelegate.swift
@@ -13,10 +13,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        
+        // 맨 처음 보여줄 ViewController
+        let mainViewController = AppleLoginViewController()
+        
+        // 맨 처음 보여줄 ViewController로 설정
+        window?.rootViewController = mainViewController
+        
+        // 화면을 띄움
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
