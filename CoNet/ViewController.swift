d575c859666d5ddbafe0429fe4707b0e6b0072ce
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-06-28 14:29:51 +0900

    Initial Commit

---
+++ CoNet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoNet
+//
+//  Created by 이안진 on 2023/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
