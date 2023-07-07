fe334e2f41f8f7bdce0bb80ee00e1bc2281461e7
slr-09 <ge12090@naver.com>
2023-07-08 03:28:31 +0900

    Refactor(signup-name): snapkit 적용

--- CoNet/Main/SignUp/EnterNameViewController.swift
+++ CoNet/Main/SignUp/EnterNameViewController.swift
@@ -5,18 +5,77 @@
 //  Created by 가은 on 2023/07/05.
 //
 
+import SnapKit
+import Then
 import UIKit
 
 class EnterNameViewController: UIViewController {
 
-    // 이름 입력 textField
-    let nameTextField = UITextField()
-    var topBar = UIView()
-    let underlineView = UIView()
-    let nameCondition1 = UILabel()
-    let eMarkView1 = UIImageView()
-    let nextBtn = UIButton()
-    let clearButton = UIButton()
+    // Component: xmark image (창 끄기)
+    let xMarkView = UIImageView().then{
+        $0.image = UIImage(systemName: "xmark")
+        $0.tintColor = UIColor.gray600
+    }
+    
+    // Component: top purple bar
+    let topBar = UIView().then{
+        $0.layer.backgroundColor = UIColor.purpleMain?.cgColor
+    }
+    
+    // Component: enter name label
+    let enterNameLabel = UILabel().then{
+        $0.text = "이름을 입력해주세요"
+        $0.font = UIFont.headline1
+    }
+    
+    // Component: 이름 입력 텍스트필드
+    let nameTextField = UITextField().then{
+        $0.placeholder = "이름 입력"
+        $0.font = UIFont.body1Regular
+        $0.tintColor = UIColor.black
+//        $0.clearButtonMode = .whileEditing
+    }
+    
+    let clearButton = UIButton().then{
+        $0.setImage(UIImage(), for: .normal)
+//        $0.addTarget(self, action: #selector(didClickClearBtn), for: .touchUpInside)
+    }
+    
+    // textfield underline
+    let underlineView = UIView().then{
+        $0.layer.backgroundColor = UIColor.gray100?.cgColor
+    }
+    
+    // Component: 느낌표 마크 1
+    let eMarkView1 = UIImageView().then{
+        $0.image = UIImage(named: "emarkPurple")
+    }
+    
+    // Component: 이름 입력 조건 label 1
+    let nameCondition1 = UILabel().then{
+        $0.text = "공백 없이 20자 이내의 한글, 영어, 숫자로 입력해주세요."
+        $0.font = UIFont.caption
+    }
+    
+    // Component: 느낌표 마크 2
+    let eMarkView2 = UIImageView().then{
+        $0.image = UIImage(named: "emarkPurple")
+    }
+    
+    // Component: 이름 입력 조건 label 2
+    let nameCondition2 = UILabel().then{
+        $0.text = "참여자 간 원활한 소통을 위해 실명을 권장합니다."
+        $0.font = UIFont.caption
+    }
+    
+    // Component: 완료 버튼
+    let nextBtn = UIButton().then{
+        $0.setTitle("완료", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = UIColor.gray200
+        $0.layer.cornerRadius = 12
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,179 +84,130 @@ class EnterNameViewController: UIViewController {
         view.backgroundColor = .white
         
         // show UI
-        showScreen()
+        layoutConstraints()
         
         // .editingChanged: editing이 될 때마다 didChangeNameTextField 함수가 호출됩니다.
         self.nameTextField.addTarget(self, action: #selector(self.didChangeNameTextField(_:)), for: .editingChanged)
         
     }
     
-    override func viewDidLayoutSubviews() {
-        
-        // nameTextField underline 그리기
-//        if underline == nil {
-//            underline = CALayer()
-//            underline!.frame = CGRectMake(0, nameTextField.frame.size.height-1, nameTextField.frame.width, 1)
-//            underline!.backgroundColor = UIColor.gray100?.cgColor
-//
-//            nameTextField.layer.addSublayer(underline!)
-//        }
-        
-        
-        
-    }
+    
     
     // show UI
-    func showScreen() {
+    func layoutConstraints() {
         
         // 안전 영역
         let safeArea = view.safeAreaLayoutGuide
         
         // Component: xmark image (창 끄기)
-        let xMarkView = UIImageView()
-        xMarkView.image = UIImage(systemName: "xmark")
-        xMarkView.tintColor = UIColor.gray600
-        
         view.addSubview(xMarkView)
         
-        // AutoResizingMask로 인한 constraint 변환을 막기 위해 false로 설정
-        xMarkView.translatesAutoresizingMaskIntoConstraints = false
-        
-        // constraint 설정
-        xMarkView.heightAnchor.constraint(equalToConstant: 24).isActive = true  // height 설정
-        xMarkView.widthAnchor.constraint(equalToConstant: 24).isActive = true   // width 설정
-        xMarkView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 21).isActive = true
-        xMarkView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 21).isActive = true
-        
-        
-        // Component: top purple bar
-        topBar.layer.backgroundColor = UIColor.purpleMain?.cgColor
-        
-        view.addSubview(topBar)
-        
-        topBar.translatesAutoresizingMaskIntoConstraints = false
-        
-        topBar.heightAnchor.constraint(equalToConstant: 4).isActive = true
-        topBar.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 0).isActive = true
-        topBar.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: 0).isActive = true
-        topBar.topAnchor.constraint(equalTo: xMarkView.bottomAnchor, constant: 14).isActive = true
-        
-        
-        // Component: main label
-        let enterNameLabel = UILabel()
-        enterNameLabel.text = "이름을 입력해주세요"
-        enterNameLabel.font = UIFont.headline1
-        
-        view.addSubview(enterNameLabel)
-        
-        enterNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        enterNameLabel.heightAnchor.constraint(equalToConstant: 36).isActive = true
-        enterNameLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 24).isActive = true
-        enterNameLabel.topAnchor.constraint(equalTo: topBar.bottomAnchor, constant: 40).isActive = true
-        
-        
-        // Component: 이름 입력 텍스트필드
-        nameTextField.placeholder = "이름 입력"
-        nameTextField.font = UIFont.body1Regular
-        nameTextField.tintColor = UIColor.black
-        nameTextField.clearButtonMode = .whileEditing
-        
-        
-        if let clearButton = nameTextField.value(forKeyPath: "clearButton") as? UIButton {
-            clearButton.setImage(UIImage(named: "clearBtn"), for: .normal)
-            clearButton.heightAnchor.constraint(equalToConstant: 16).isActive = true
-            clearButton.widthAnchor.constraint(equalToConstant: 16).isActive = true
+        xMarkView.snp.makeConstraints{ make in
+            make.height.equalTo(24)
+            make.width.equalTo(24)
+            make.leading.equalTo(safeArea.snp.leading).offset(21)
+            make.top.equalTo(safeArea.snp.top).offset(21)
         }
         
         
-        view.addSubview(nameTextField)
-        nameTextField.translatesAutoresizingMaskIntoConstraints = false
+        // Component: top purple bar
+        view.addSubview(topBar)
         
-        nameTextField.heightAnchor.constraint(equalToConstant: 20).isActive = true
-        nameTextField.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 24).isActive = true
-        nameTextField.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -24).isActive = true
-        nameTextField.topAnchor.constraint(equalTo: enterNameLabel.bottomAnchor, constant: 42).isActive = true
+        topBar.snp.makeConstraints{ make in
+            make.height.equalTo(4)
+            make.leading.equalTo(safeArea.snp.leading).offset(0)
+            make.trailing.equalTo(safeArea.snp.trailing).offset(0)
+            make.top.equalTo(xMarkView.snp.bottom).offset(14)
+        }
+        
+        // Component: main label
+        view.addSubview(enterNameLabel)
+        
+        enterNameLabel.snp.makeConstraints{ make in
+            make.height.equalTo(36)
+            make.leading.equalTo(safeArea.snp.leading).offset(24)
+            make.top.equalTo(topBar.snp.bottom).offset(40)
+        }
+        
+        
+        // Component: nametextfield
+        view.addSubview(nameTextField)
+        
+        nameTextField.snp.makeConstraints{ make in
+            make.height.equalTo(20)
+            make.leading.equalTo(safeArea.snp.leading).offset(24)
+            make.trailing.equalTo(safeArea.snp.trailing).offset(-24)
+            make.top.equalTo(enterNameLabel.snp.bottom).offset(42)
+        }
         
         // textfield underline
-        underlineView.layer.backgroundColor = UIColor.gray100?.cgColor
-        
         view.addSubview(underlineView)
         
-        underlineView.translatesAutoresizingMaskIntoConstraints = false
+        underlineView.snp.makeConstraints{ make in
+            make.height.equalTo(1)
+            make.leading.equalTo(safeArea.snp.leading).offset(24)
+            make.trailing.equalTo(safeArea.snp.trailing).offset(-24)
+            make.top.equalTo(nameTextField.snp.bottom).offset(10)
+        }
         
-        underlineView.heightAnchor.constraint(equalToConstant: 1).isActive = true
-        underlineView.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 10).isActive = true
-        underlineView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 24).isActive = true
-        underlineView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -24).isActive = true
+        // textfield clear button
+        view.addSubview(clearButton)
         
+        clearButton.snp.makeConstraints{ make in
+            make.width.equalTo(16)
+            make.height.equalTo(16)
+            make.trailing.equalTo(nameTextField.snp.trailing).offset(0)
+            make.top.equalTo(nameTextField.snp.top).offset(2)
+            make.bottom.equalTo(nameTextField.snp.bottom).offset(-2)
+        }
         
         // Component: 느낌표 마크 1
-        eMarkView1.image = UIImage(named: "emarkPurple")
-        
         view.addSubview(eMarkView1)
         
-        eMarkView1.translatesAutoresizingMaskIntoConstraints = false
-        
-        eMarkView1.heightAnchor.constraint(equalToConstant: 12).isActive = true
-        eMarkView1.widthAnchor.constraint(equalToConstant: 12).isActive = true
-        eMarkView1.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 24).isActive = true
-        eMarkView1.topAnchor.constraint(equalTo: underlineView.bottomAnchor, constant: 12).isActive = true
+        eMarkView1.snp.makeConstraints{ make in
+            make.height.equalTo(12)
+            make.width.equalTo(12)
+            make.leading.equalTo(safeArea.snp.leading).offset(24)
+            make.top.equalTo(underlineView.snp.bottom).offset(12)
+        }
         
         // Component: 이름 입력 조건 label 1
-        nameCondition1.text = "공백 없이 20자 이내의 한글, 영어, 숫자로 입력해주세요."
-        nameCondition1.font = UIFont.caption
-        
         view.addSubview(nameCondition1)
         
-        nameCondition1.translatesAutoresizingMaskIntoConstraints = false
-        
-        nameCondition1.heightAnchor.constraint(equalToConstant: 16).isActive = true
-        nameCondition1.leadingAnchor.constraint(equalTo: eMarkView1.trailingAnchor, constant: 5).isActive = true
-        nameCondition1.topAnchor.constraint(equalTo: underlineView.bottomAnchor, constant: 10).isActive = true
+        nameCondition1.snp.makeConstraints{ make in
+            make.height.equalTo(16)
+            make.leading.equalTo(eMarkView1.snp.trailing).offset(5)
+            make.top.equalTo(underlineView.snp.bottom).offset(10)
+        }
         
         // Component: 느낌표 마크 2
-        let eMarkView2 = UIImageView()
-        eMarkView2.image = UIImage(named: "emarkPurple")
-        
         view.addSubview(eMarkView2)
         
-        eMarkView2.translatesAutoresizingMaskIntoConstraints = false
-        
-        eMarkView2.heightAnchor.constraint(equalToConstant: 12).isActive = true
-        eMarkView2.widthAnchor.constraint(equalToConstant: 12).isActive = true
-        eMarkView2.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 24).isActive = true
-        eMarkView2.topAnchor.constraint(equalTo: eMarkView1.bottomAnchor, constant: 8).isActive = true
+        eMarkView2.snp.makeConstraints{ make in
+            make.height.equalTo(12)
+            make.width.equalTo(12)
+            make.leading.equalTo(safeArea.snp.leading).offset(24)
+            make.top.equalTo(eMarkView1.snp.bottom).offset(8)
+        }
         
         // Component: 이름 입력 조건 label 2
-        let nameCondition2 = UILabel()
-        nameCondition2.text = "참여자 간 원활한 소통을 위해 실명을 권장합니다."
-        nameCondition2.font = UIFont.caption
         view.addSubview(nameCondition2)
         
-        nameCondition2.translatesAutoresizingMaskIntoConstraints = false
-        
-        nameCondition2.heightAnchor.constraint(equalToConstant: 16).isActive = true
-        nameCondition2.leadingAnchor.constraint(equalTo: eMarkView2.trailingAnchor, constant: 5).isActive = true
-        nameCondition2.topAnchor.constraint(equalTo: nameCondition1.bottomAnchor, constant: 4).isActive = true
-        
+        nameCondition2.snp.makeConstraints{ make in
+            make.height.equalTo(16)
+            make.leading.equalTo(eMarkView2.snp.trailing).offset(5)
+            make.top.equalTo(nameCondition1.snp.bottom).offset(4)
+        }
         
         // Component: 완료 버튼
-        nextBtn.setTitle("완료", for: .normal)
-        nextBtn.setTitleColor(.white, for: .normal)
-        nextBtn.backgroundColor = UIColor.gray200
-        
         view.addSubview(nextBtn)
         
-        nextBtn.translatesAutoresizingMaskIntoConstraints = false
-        
-        nextBtn.heightAnchor.constraint(equalToConstant: 52).isActive = true
-        nextBtn.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 24).isActive = true
-        nextBtn.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -24).isActive = true
-        nextBtn.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -46).isActive = true
-        
-        nextBtn.layer.cornerRadius = 12
-        
+        nextBtn.snp.makeConstraints{ make in
+            make.height.equalTo(52)
+            make.leading.equalTo(safeArea.snp.leading).offset(24)
+            make.trailing.equalTo(safeArea.snp.trailing).offset(-24)
+            make.bottom.equalTo(safeArea.snp.bottom).offset(-46)
+        }
         
     }
     
@@ -211,6 +221,8 @@ class EnterNameViewController: UIViewController {
             underlineView.layer.backgroundColor = UIColor.gray100?.cgColor
             nameCondition1.textColor = UIColor.black
             eMarkView1.image = UIImage(named: "emarkPurple")
+            nextBtn.layer.backgroundColor = UIColor.gray200?.cgColor
+            clearButton.setImage(UIImage(), for: .normal)
             return true
         }
         
@@ -219,9 +231,7 @@ class EnterNameViewController: UIViewController {
         guard let _ = editText?.range(of: regexPattern, options: .regularExpression)
         else {
             // 조건 만족하지 않을 경우 return false
-            if let clearButton = nameTextField.value(forKeyPath: "clearButton") as? UIButton {
-                clearButton.setImage(UIImage(named: "emarkRed"), for: .normal)
-            }
+            clearButton.setImage(UIImage(named: "emarkRedEmpty"), for: .normal)
             underlineView.layer.backgroundColor = UIColor.error?.cgColor
             nameCondition1.textColor = UIColor.error
             eMarkView1.image = UIImage(named: "emarkRed")
@@ -231,17 +241,20 @@ class EnterNameViewController: UIViewController {
         }
         
         // 조건 만족한 경우 return true
-        if let clearButton = nameTextField.value(forKeyPath: "clearButton") as? UIButton {
-            clearButton.setImage(UIImage(named: "clearBtn"), for: .normal)
-            clearButton.heightAnchor.constraint(equalToConstant: 16).isActive = true
-            clearButton.widthAnchor.constraint(equalToConstant: 16).isActive = true
-        }
+        clearButton.setImage(UIImage(named: "clearBtn"), for: .normal)
+        clearButton.addTarget(self, action: #selector(didClickClearButton), for: .touchUpInside)
         underlineView.layer.backgroundColor = UIColor.purpleMain?.cgColor
         nameCondition1.textColor = UIColor.black
         eMarkView1.image = UIImage(named: "emarkPurple")
-        
         nextBtn.layer.backgroundColor = UIColor.purpleMain?.cgColor
         
         return true
     }
+    
+    // 이름 입력 텍스트필드의 클리어 버튼을 클릭했을 때
+    // 입력된 텍스트를 지웁니다.
+    @objc func didClickClearButton() {
+        nameTextField.text = ""
+    }
+    
 }
