4d50846f7ebd6e13539d481568c0136d71abe06b
slr-09 <ge12090@naver.com>
2023-07-08 15:50:55 +0900

    Feat(tabbar): 탭바 구현

--- CoNet/Main/TabbarViewController.swift
+++ CoNet/Main/TabbarViewController.swift
@@ -9,21 +9,52 @@ import UIKit
 
 class TabbarViewController: UITabBarController {
 
+    // 초기 인덱스를 0으로 설정합니다.
+    // 아이템이 선택되면 그 아이템의 인덱스가 defaultIndex에 저장됩니다.
+    var defaultIndex = 0 {
+        didSet {
+            self.selectedIndex = defaultIndex
+        }
+    }
+    
+    // 홈탭
+    let firstNVController = UINavigationController().then{
+        let firstTabController = ViewController()
+        $0.addChild(firstTabController)
+        $0.tabBarItem.image = UIImage(named: "tabbarHome")
+        $0.tabBarItem.selectedImage = UIImage(named: "tabbarHomeSelected")
+        $0.tabBarItem.title = "홈"
+    }
+    
+    // 모임탭
+    let secondNVController = UINavigationController().then{
+        let secondTabController = ViewController()
+        $0.addChild(secondTabController)
+        $0.tabBarItem.image = UIImage(named: "tabbarMeeting")
+        $0.tabBarItem.selectedImage = UIImage(named: "tabbarMeetingSelected")
+        $0.tabBarItem.title = "모임"
+    }
+    
+    // 마이페이지 탭
+    let thirdNVController = UINavigationController().then{
+        let thirdTabController = ViewController()
+        $0.addChild(thirdTabController)
+        $0.tabBarItem.image = UIImage(named: "tabbarMypage")
+        $0.tabBarItem.selectedImage = UIImage(named: "tabbarMypageSelected")
+        $0.tabBarItem.title = "MY"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        self.selectedIndex = defaultIndex
+        
+        let viewControllers = [firstNVController, secondNVController, thirdNVController]
+        self.setViewControllers(viewControllers, animated: true)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
