e23f036a7a912c2e6843d24800559819636698b7
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-07-08 17:38:38 +0900

    Feat(my-page): 회원 정보 UI 구현

--- CoNet/Main/MyPage/MyPageViewController.swift
+++ CoNet/Main/MyPage/MyPageViewController.swift
@@ -17,9 +17,61 @@ class MyPageViewController: UIViewController {
         $0.textColor = UIColor.textHigh
     }
     
+    let profileImage = UIImageView().then {
+        $0.image = UIImage(systemName: "person.circle.fill")
+        $0.tintColor = .gray100
+    }
+    
+    let nameLabel = UILabel().then {
+        $0.text = "이안진"
+        $0.font = UIFont.headline2Bold
+        $0.textColor = UIColor.textHigh
+    }
+    
+    let divider = UIView().then {
+        $0.backgroundColor = UIColor.gray100
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // background color를 white로 설정 (default: black)
+        view.backgroundColor = .white
+        
+        // show UI
+        layoutConstraints()
     }
-
+    
+    // show UI
+    func layoutConstraints() {
+        
+        // 안전 영역
+        let safeArea = view.safeAreaLayoutGuide
+        
+        // Component: xmark image (창 끄기)
+        view.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.height.equalTo(36)
+            make.top.equalTo(safeArea.snp.top).offset(40)
+            make.leading.equalTo(safeArea.snp.leading).offset(24)
+        }
+        
+        view.addSubview(profileImage)
+        
+        profileImage.snp.makeConstraints { make in
+            make.width.equalTo(60)
+            make.height.equalTo(60)
+            make.top.equalTo(titleLabel.snp.bottom).offset(32)
+            make.leading.equalTo(safeArea.snp.leading).offset(24)
+        }
+        
+        view.addSubview(nameLabel)
+        
+        nameLabel.snp.makeConstraints { make in
+            make.height.equalTo(26)
+            make.centerY.equalTo(profileImage)
+            make.leading.equalTo(profileImage.snp.trailing).offset(20)
+        }
+    }
 }
