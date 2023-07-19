ae40886c2c6e9bdade6b7597700614c391efac99
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-07-20 03:51:54 +0900

    Refactor(my-page): MyPageViewController refactoring

--- CoNet/Main/MyPage/MyPageViewController.swift
+++ CoNet/Main/MyPage/MyPageViewController.swift
@@ -10,7 +10,7 @@ import Then
 import UIKit
 
 class MyPageViewController: UIViewController {
-    // title
+    // "MY" 타이틀
     let titleLabel = UILabel().then {
         $0.text = "MY"
         $0.font = UIFont.headline1
@@ -18,9 +18,7 @@ class MyPageViewController: UIViewController {
     }
     
     // 프로필 이미지 - 현재 기본 이미지로 보여줌
-    let profileImage = UIImageView().then {
-        $0.image = UIImage(named: "defaultProfile")
-    }
+    let profileImage = UIImageView().then { $0.image = UIImage(named: "defaultProfile") }
     
     // 이름
     let nameLabel = UILabel().then {
@@ -29,21 +27,18 @@ class MyPageViewController: UIViewController {
         $0.textColor = UIColor.textHigh
     }
     
+    // 구분선
     let divider = UIView().then { $0.backgroundColor = UIColor.gray50 }
     let shortDivider = UIView().then { $0.backgroundColor = UIColor.gray100 }
+    let secondShortDivider = UIView().then { $0.backgroundColor = UIColor.gray100 }
     
+    // 마이페이지 리스트
     let myPageList = MyPageList()
-    let userInfoButton = UIButton().then { $0.backgroundColor = .clear }
-    
     lazy var userInfoView = myPageList.arrowView(title: "회원정보", labelFont: UIFont.body1Regular!)
     lazy var notificationView = myPageList.toggleView(title: "알림 설정")
-    
     lazy var noticeView = myPageList.arrowView(title: "공지사항", labelFont: UIFont.body1Regular!)
     lazy var inquireView = myPageList.arrowView(title: "문의하기", labelFont: UIFont.body1Regular!)
     lazy var termView = myPageList.noArrowView(title: "이용약관")
-    
-    let secondShortDivider = UIView().then { $0.backgroundColor = UIColor.gray100 }
-    
     lazy var logoutView = myPageList.noArrowView(title: "로그아웃")
     
     override func viewDidLoad() {
@@ -53,42 +48,45 @@ class MyPageViewController: UIViewController {
         // background color를 white로 설정 (default: black)
         view.backgroundColor = .white
         
-        // show UI
-        titleLayoutConstraints()
+        // 전체 layout constraints
         layoutConstraints()
         
-        userInfoView.addTarget(self, action: #selector(didClickNextButton(_:)), for: .touchUpInside)
+        userInfoView.addTarget(self, action: #selector(showUserInfoViewController(_:)), for: .touchUpInside)
         noticeView.addTarget(self, action: #selector(showNoticeViewController(_:)), for: .touchUpInside)
         inquireView.addTarget(self, action: #selector(showInquireViewController(_:)), for: .touchUpInside)
-        termView.addTarget(self, action: #selector(didClickNextButton(_:)), for: .touchUpInside)
     }
     
-    // TODO: method 통일 시키기
-    @objc func didClickNextButton(_ sender: UIView) {
+    @objc private func showUserInfoViewController(_ sender: UIView) {
         let nextVC = UserInfoViewController()
         nextVC.hidesBottomBarWhenPushed = true
         navigationController?.pushViewController(nextVC, animated: true)
         
     }
     
-    @objc func showNoticeViewController(_ sender: UIView) {
+    @objc private func showNoticeViewController(_ sender: UIView) {
         let nextVC = NoticeViewController()
         navigationController?.pushViewController(nextVC, animated: true)
         
     }
     
-    @objc func showInquireViewController(_ sender: UIView) {
+    @objc private func showInquireViewController(_ sender: UIView) {
         let nextVC = InquireViewController()
         navigationController?.pushViewController(nextVC, animated: true)
         
     }
     
-    func titleLayoutConstraints() {
+    // 전체 layout constraints
+    private func layoutConstraints() {
+        titleConstraints()
+        userConstraints()
+        contentsConstraints()
+    }
+    
+    // "MY" 타이틀 constraints
+    private func titleConstraints() {
         let safeArea = view.safeAreaLayoutGuide
         
-        // Component: xmark image (창 끄기)
         view.addSubview(titleLabel)
-        
         titleLabel.snp.makeConstraints { make in
             make.height.equalTo(36)
             make.top.equalTo(safeArea.snp.top).offset(40)
@@ -96,14 +94,11 @@ class MyPageViewController: UIViewController {
         }
     }
     
-    // show UI
-    func layoutConstraints() {
-        
-        // 안전 영역
+    // 프로필 이미지, 이름 constraints
+    private func userConstraints() {
         let safeArea = view.safeAreaLayoutGuide
         
         view.addSubview(profileImage)
-        
         profileImage.snp.makeConstraints { make in
             make.width.equalTo(60)
             make.height.equalTo(60)
@@ -112,7 +107,6 @@ class MyPageViewController: UIViewController {
         }
         
         view.addSubview(nameLabel)
-        
         nameLabel.snp.makeConstraints { make in
             make.height.equalTo(26)
             make.centerY.equalTo(profileImage)
@@ -120,15 +114,18 @@ class MyPageViewController: UIViewController {
         }
         
         view.addSubview(divider)
-        
         divider.snp.makeConstraints { make in
             make.height.equalTo(18)
             make.width.equalTo(safeArea.snp.width)
             make.top.equalTo(profileImage.snp.bottom).offset(22)
         }
+    }
+    
+    // 마이페이지 리스트 contents constraints
+    private func contentsConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
         
         view.addSubview(userInfoView)
-        
         userInfoView.snp.makeConstraints { make in
             make.width.equalTo(safeArea.snp.width).offset(-48)
             make.top.equalTo(divider.snp.bottom).offset(32)
@@ -138,13 +135,9 @@ class MyPageViewController: UIViewController {
         myPageListLayoutConstraints(notificationView, previousView: userInfoView)
         
         view.addSubview(shortDivider)
-        
         shortDivider.snp.makeConstraints { make in
-            make.height.equalTo(1)
-            make.width.equalTo(safeArea.snp.width).offset(-48)
-            
+            dividerConstraints(make: make)
             make.top.equalTo(notificationView.snp.bottom).offset(24)
-            verticalPadding(make: make)
         }
         
         myPageListLayoutConstraints(noticeView, previousView: shortDivider)
@@ -152,23 +145,19 @@ class MyPageViewController: UIViewController {
         myPageListLayoutConstraints(termView, previousView: inquireView)
         
         view.addSubview(secondShortDivider)
-
         secondShortDivider.snp.makeConstraints { make in
-            make.height.equalTo(1)
-            make.width.equalTo(safeArea.snp.width).offset(-48)
-
+            dividerConstraints(make: make)
             make.top.equalTo(termView.snp.bottom).offset(24)
-            verticalPadding(make: make)
         }
         
         myPageListLayoutConstraints(logoutView, previousView: secondShortDivider)
     }
     
-    func myPageListLayoutConstraints(_ listView: UIView, previousView: UIView) {
+    // 리스트의 공통된 constraints
+    private func myPageListLayoutConstraints(_ listView: UIView, previousView: UIView) {
         let safeArea = view.safeAreaLayoutGuide
         
         view.addSubview(listView)
-
         listView.snp.makeConstraints { make in
             make.width.equalTo(safeArea.snp.width).offset(-48)
             make.top.equalTo(previousView.snp.bottom).offset(24)
@@ -176,7 +165,17 @@ class MyPageViewController: UIViewController {
         }
     }
     
-    func verticalPadding(make: ConstraintMaker) {
+    // 얇은 구분선 constraints
+    private func dividerConstraints(make: ConstraintMaker) {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        make.height.equalTo(1)
+        make.width.equalTo(safeArea.snp.width).offset(-48)
+        verticalPadding(make: make)
+    }
+    
+    // 왼쪽, 오른쪽 여백 24
+    private func verticalPadding(make: ConstraintMaker) {
         let safeArea = view.safeAreaLayoutGuide
         
         make.leading.equalTo(safeArea.snp.leading).offset(24)
