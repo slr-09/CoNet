2be4eaaf88d51381f5811c405780c7acf1ed31bb
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-07-22 16:48:24 +0900

    Refactor: PlanListCollectionView 파일 분리

--- CoNet/Main/Meeting/Plan/WaitingPlanListViewController.swift
+++ CoNet/Main/Meeting/Plan/WaitingPlanListViewController.swift
@@ -124,40 +124,6 @@ class WaitingPlanCell: UICollectionViewCell {
     }
 }
 
-class PlanListCollectionView: UIView {
-    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        
-        $0.backgroundColor = UIColor.gray50
-        $0.contentInset = UIEdgeInsets.init(top: 10, left: 0, bottom: 10, right: 0)
-        $0.showsHorizontalScrollIndicator = false
-        $0.collectionViewLayout = layout
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setup()
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        setup()
-    }
-    
-    func setup() {
-        collectionView.showsVerticalScrollIndicator = false
-        
-        addSubview(collectionView)
-        collectionView.snp.makeConstraints { (make) in
-            make.height.equalToSuperview()
-            make.leading.equalTo(24)
-            make.trailing.equalTo(-24)
-            make.centerY.equalToSuperview()
-        }
-    }
-}
-
 class WaitingPlanListViewController: UIViewController {
     private lazy var mainView = PlanListCollectionView.init(frame: self.view.frame)
 
