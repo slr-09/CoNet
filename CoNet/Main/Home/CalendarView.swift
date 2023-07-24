3e694c2a50e6c2d796c8f02061f4e03422b3c82d
slr-09 <ge12090@naver.com>
2023-07-24 21:45:51 +0900

    Feat(home): 날짜 선택 시 label 변화 (error)

--- CoNet/Main/Home/CalendarView.swift
+++ CoNet/Main/Home/CalendarView.swift
@@ -157,7 +157,7 @@ class CalendarView: UIView {
         yearMonth.setTitle(header, for: .normal)
     }
     
-    // 달 이동 
+    // 달 이동
     func moveMonth(month: Int) {
         let header = calendarDateFormatter.moveMonth(month: month)
         calendarCollectionView.reloadData()
@@ -166,6 +166,7 @@ class CalendarView: UIView {
 }
 
 extension CalendarView: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
     // 셀 수
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return calendarDateFormatter.days.count
@@ -218,5 +219,31 @@ extension CalendarView: UICollectionViewDataSource, UICollectionViewDelegate, UI
         }
         
         return cell
+    }
+    
+    // 각 셀을 클릭했을 때 이벤트 처리
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected cell at indexPath: \(indexPath)")
+        
+        let home = HomeViewController()
+        
+        let format = DateFormatter()
+        format.dateFormat = "dd"
+        
+        // 오늘 날짜 계산
+        let today = format.string(from: Date())
+        
+        format.dateFormat = "MM"
+        // 오늘 날짜 month 계산
+        let todayMonth = format.string(from: Date())
+        
+        // 달력 month
+        let calendarMonth = calendarDateFormatter.currentMonth()
+        
+        if calendarDateFormatter.days[indexPath.item] == today && todayMonth == calendarMonth {
+            home.changeDate(month: "", day: "")
+        } else {
+            home.changeDate(month: calendarMonth, day: calendarDateFormatter.days[indexPath.item])
+        }
     }
 }
