304b6fcffec3f4fc4090c74959508b91be09c08e
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-07-25 23:46:51 +0900

    Feat(meeting-main): white gradient view 추가

--- CoNet/Main/Meeting/MeetingMainViewController.swift
+++ CoNet/Main/Meeting/MeetingMainViewController.swift
@@ -10,25 +10,17 @@ import Then
 import UIKit
 
 class MeetingMainViewController: UIViewController {
-    
-    let scrollview = UIScrollView().then {
-        $0.backgroundColor = .clear
-    }
-    
-    let contentView = UIView().then {
-        $0.backgroundColor = .clear
-    }
+    let scrollview = UIScrollView().then { $0.backgroundColor = .clear }
+    let contentView = UIView().then { $0.backgroundColor = .clear }
     
     // 사이드바 버튼
-    let sidebarButton = UIButton().then {
-        $0.setImage(UIImage(named: "sidebarButton"), for: .normal)
-    }
+    let sidebarButton = UIButton().then { $0.setImage(UIImage(named: "sidebarButton"), for: .normal) }
     
     // 뒤로가기 버튼
-    let backButton = UIButton().then {
-        $0.setImage(UIImage(named: "meetingMainBackButton"), for: .normal)
-    }
+    let backButton = UIButton().then { $0.setImage(UIImage(named: "meetingMainBackButton"), for: .normal) }
     
+    // 상단 모임 이미지
+    let whiteGradientView = WhiteGradientView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
     let meetingImage = UIImageView().then {
         $0.image = UIImage(named: "space")
         $0.clipsToBounds = true
@@ -37,26 +29,29 @@ class MeetingMainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 배경색 .white로 지정
         view.backgroundColor = .white
         
+        // navigation bar title "iOS 스터디"로 지정
         self.navigationController?.navigationBar.isHidden = false
         navigationItem.title = "iOS 스터디"
         
-        sidebarButton.addTarget(self, action: #selector(sidebarButtonTapped), for: .touchUpInside)
-        // UIBarButtonItem 생성
-        let barButtonItem = UIBarButtonItem(customView: sidebarButton)
-        
-        // 네비게이션 바에 추가
-        navigationItem.rightBarButtonItem = barButtonItem
-        
-        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
-        // UIBarButtonItem 생성
-        let leftbarButtonItem = UIBarButtonItem(customView: backButton)
-        
-        // 네비게이션 바에 추가
-        navigationItem.leftBarButtonItem = leftbarButtonItem
+        // 네비게이션 바 item 추가 - 뒤로가기, 사이드바 버튼
+        addNavigationBarItem()
         
         layoutContraints()
+    }
+    
+    private func addNavigationBarItem() {
+        // 사이드바 버튼 추가
+        sidebarButton.addTarget(self, action: #selector(sidebarButtonTapped), for: .touchUpInside)
+        let barButtonItem = UIBarButtonItem(customView: sidebarButton)
+        navigationItem.rightBarButtonItem = barButtonItem
+        
+        // 뒤로가기 버튼 추가
+        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        let leftbarButtonItem = UIBarButtonItem(customView: backButton)
+        navigationItem.leftBarButtonItem = leftbarButtonItem
     }
     
     @objc private func backButtonTapped() {
@@ -69,7 +64,14 @@ class MeetingMainViewController: UIViewController {
         print("Image Button Tapped!")
     }
     
+    // 전체 layout constraints
     private func layoutContraints() {
+        scrollviewConstraints() // 스크롤뷰
+        imageConstraints() // 상단 이미지
+    }
+    
+    // scrollview 추가
+    private func scrollviewConstraints() {
         view.addSubview(scrollview)
         scrollview.snp.makeConstraints { make in
             make.edges.equalToSuperview().inset(0)
@@ -81,15 +83,21 @@ class MeetingMainViewController: UIViewController {
             make.width.equalTo(scrollview.frameLayoutGuide)
             make.height.equalTo(2000)
         }
-        
-        imageConstaints()
     }
-
-    private func imageConstaints() {
+    
+    // 상단 이미지 constraints
+    private func imageConstraints() {
         contentView.addSubview(meetingImage)
         meetingImage.snp.makeConstraints { make in
             make.width.equalToSuperview()
             make.height.equalTo(contentView.snp.width)
+            make.top.equalTo(scrollview.snp.top).offset(-98)
+        }
+        
+        contentView.addSubview(whiteGradientView)
+        whiteGradientView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalTo(100)
             make.top.equalTo(scrollview.snp.top).offset(-98)
         }
     }
