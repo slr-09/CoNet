47f91fbcc11c015cd43c15e78bd7095df0abe54d
slr-09 <ge12090@naver.com>
2023-07-27 15:21:32 +0900

    Feat(meeting-main): 모임-메인 캘린더뷰까지

--- CoNet/Main/Meeting/MeetingMainViewController.swift
+++ CoNet/Main/Meeting/MeetingMainViewController.swift
@@ -26,6 +26,46 @@ class MeetingMainViewController: UIViewController {
         $0.clipsToBounds = true
     }
     
+    // 즐겨찾기 버튼
+    let starButton = UIImageView().then {
+        $0.image = UIImage(named: "meetingStarOff")
+    }
+    
+    // 모임 이름
+    let meetingName = UILabel().then {
+        $0.text = "iOS 스터디"
+        $0.font = UIFont.headline1
+        $0.textColor = UIColor.textHigh
+    }
+    
+    // 약속 만들기 버튼
+    let addMeetingButton = UIButton().then {
+        $0.setTitle("약속 만들기", for: .normal)
+        $0.setTitleColor(UIColor.textHigh, for: .normal)
+        $0.titleLabel?.font = UIFont.body2Medium
+        $0.layer.cornerRadius = 16.5
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.purpleMain?.cgColor
+    }
+    
+    // 멤버 이미지
+    let memberImage = UIImageView().then {
+        $0.image = UIImage(named: "meetingMember")
+    }
+    
+    // 멤버 수
+    let memberNum = UILabel().then {
+        $0.text = "n명"
+        $0.textColor = UIColor.textMedium
+        $0.font = UIFont.body1Medium
+    }
+    
+    // 캘린더뷰
+    let calendarView = CalendarView().then {
+        $0.layer.borderWidth = 0.2
+        $0.layer.borderColor = UIColor.gray300?.cgColor
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -33,7 +73,7 @@ class MeetingMainViewController: UIViewController {
         view.backgroundColor = .white
         
         // navigation bar title "iOS 스터디"로 지정
-        self.navigationController?.navigationBar.isHidden = false
+        navigationController?.navigationBar.isHidden = false
         navigationItem.title = "iOS 스터디"
         
         // 네비게이션 바 item 추가 - 뒤로가기, 사이드바 버튼
@@ -44,12 +84,12 @@ class MeetingMainViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        self.navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.isHidden = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.navigationController?.navigationBar.isHidden = false
+        navigationController?.navigationBar.isHidden = false
     }
     
     private func addNavigationBarItem() {
@@ -80,8 +120,10 @@ class MeetingMainViewController: UIViewController {
     
     // 전체 layout constraints
     private func layoutContraints() {
-        scrollviewConstraints() // 스크롤뷰
-        imageConstraints() // 상단 이미지
+        scrollviewConstraints()     // 스크롤뷰
+        imageConstraints()          // 상단 이미지
+        headerConstraints()         // 모임 정보
+        calendarViewConstraints()   // 캘린더 뷰
     }
     
     // scrollview 추가
@@ -115,6 +157,61 @@ class MeetingMainViewController: UIViewController {
             make.top.equalTo(scrollview.snp.top).offset(-98)
         }
     }
+    
+    // 캘린더 뷰 위 모임 정보 constraints
+    private func headerConstraints() {
+        // 즐겨찾기 버튼
+        contentView.addSubview(starButton)
+        starButton.snp.makeConstraints { make in
+            make.height.width.equalTo(50)
+            make.leading.equalTo(contentView.snp.leading)
+            make.centerY.equalTo(meetingImage.snp.bottom)
+        }
+        
+        // 모임 이름
+        contentView.addSubview(meetingName)
+        meetingName.snp.makeConstraints { make in
+            make.height.equalTo(36)
+            make.leading.equalTo(contentView.snp.leading).offset(24)
+            make.top.equalTo(starButton.snp.bottom).offset(12)
+        }
+        
+        // 약속 만들기 버튼
+        contentView.addSubview(addMeetingButton)
+        addMeetingButton.snp.makeConstraints { make in
+            make.width.equalTo(103)
+            make.height.equalTo(33)
+            make.centerY.equalTo(meetingName.snp.centerY)
+            make.trailing.equalTo(contentView.snp.trailing).offset(-24)
+        }
+        
+        // 멤버 이미지
+        contentView.addSubview(memberImage)
+        memberImage.snp.makeConstraints { make in
+            make.width.height.equalTo(16)
+            make.top.equalTo(meetingName.snp.bottom).offset(6)
+            make.leading.equalTo(contentView.snp.leading).offset(24)
+        }
+        
+        // 멤버 수
+        contentView.addSubview(memberNum)
+        memberNum.snp.makeConstraints { make in
+            make.height.equalTo(24)
+            make.leading.equalTo(memberImage.snp.trailing).offset(4)
+            make.centerY.equalTo(memberImage.snp.centerY)
+        }
+    }
+    
+    // 캘린더뷰
+    func calendarViewConstraints() {
+        contentView.addSubview(calendarView)
+        calendarView.snp.makeConstraints { make in
+            make.top.equalTo(memberImage.snp.bottom).offset(40)
+            make.leading.trailing.equalTo(contentView)
+            make.height.equalTo(443)
+        }
+    }
+    
 }
 
 extension MeetingMainViewController: ModalViewControllerDelegate {
