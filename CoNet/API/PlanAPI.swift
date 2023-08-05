581f6a50864c489dd91cd226c4a3974dc06cbfdf
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-08-05 23:17:55 +0900

    Feat(plan-api): 팀 내 대기중인 약속 api 구현

---
+++ CoNet/API/PlanAPI.swift
@@ -0,0 +1,47 @@
+//
+//  PlanAPI.swift
+//  CoNet
+//
+//  Created by 이안진 on 2023/08/05.
+//
+
+import Alamofire
+import Foundation
+import KeychainSwift
+import UIKit
+
+struct GetWaitingPlansAtMeetingResult: Codable {
+    let count: Int
+    let plans: [WaitingPlans]
+}
+
+struct WaitingPlans: Codable {
+    let planId: Int
+    let startDate, endDate, teamName, planName: String
+}
+
+class PlanAPI {
+    let keychain = KeychainSwift()
+    let baseUrl = "http://15.164.196.172:9000"
+    
+    // 팀 내 대기중인 약속 조회
+    func getWaitingPlansAtMeeting(meetingId: Int, completion: @escaping (_ count: Int, _ plans: [WaitingPlans]) -> Void) {
+        let url = "\(baseUrl)/team/plan/waiting?teamId=\(meetingId)"
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/json"
+        ]
+        
+        AF.request(url, method: .get, encoding: JSONEncoding.default, headers: headers)
+            .responseDecodable(of: BaseResponse<GetWaitingPlansAtMeetingResult>.self) { response in
+                switch response.result {
+                case .success(let response):
+                    guard let count = response.result?.count else { return }
+                    guard let serverPlans = response.result?.plans else { return }
+                    completion(count, serverPlans)
+                    
+                case .failure(let error):
+                    print("DEBUG(팀 내 대기 중인 약속 api) error: \(error)")
+                }
+            }
+    }
+}
