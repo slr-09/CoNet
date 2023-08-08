22c1de823a7123f092caa2ce59dda8f0bd37c668
slr-09 <ge12090@naver.com>
2023-08-08 23:15:01 +0900

    Feat(plan): 약속 확정 api 생성

--- CoNet/API/PlanAPI.swift
+++ CoNet/API/PlanAPI.swift
@@ -273,5 +273,31 @@ class PlanAPI {
             }
         }
     }
+    
+    // 약속 확정
+    func fixPlan(planId: Int, fixedDate: String, fixedTime: String, userId: [Int]) {
+        let url = "\(baseUrl)/team/plan/delete"
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/json"
+        ]
+        
+        let parameters: Parameters = [
+            "planId": planId,
+            "fixed_date": fixedDate,
+            "fixed_time": fixedTime,
+            "userId": userId
+        ]
+        
+        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
+        .responseDecodable(of: BaseResponse<String>.self) { response in
+            switch response.result {
+            case .success(let response):
+                print("DEBUG(약속 삭제 api) success response: \(response)")
+                
+            case .failure(let error):
+                print("DEBUG(약속 삭제 api) error: \(error)")
+            }
+        }
+    }
 }
     
