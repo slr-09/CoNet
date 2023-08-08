a7132dec250117167550ff8c2606351005b9fdcf
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-08-08 15:26:13 +0900

    Feat(decided-plan): 확정된 약속 상세 페이지 화면 구현

---
+++ CoNet/Main/Meeting/PlanList/PlanDetail/DecidedPlanInfoViewController.swift
@@ -0,0 +1,218 @@
+//
+//  DecidedPlanInfoViewController.swift
+//  CoNet
+//
+//  Created by 이안진 on 2023/08/08.
+//
+
+import UIKit
+
+class DecidedPlanInfoViewController: UIViewController {
+    var planId: Int = 0
+    
+    // 배경 - .clear
+    let scrollview = UIScrollView().then { $0.backgroundColor = .clear }
+    let contentsView = UIView().then { $0.backgroundColor = .clear }
+    
+    // 우측 상단 점 세개 아이콘 - bottom sheet 버튼
+    let bottomSheetButton = UIButton().then { $0.setImage(UIImage(named: "sidebar"), for: .normal) }
+    
+    // 약속 이름
+    let nameRow = PlanInfoRow().then {
+        $0.setLabel("약속 이름")
+        $0.setText("모임 입니당")
+    }
+    
+    // 약속 날짜
+    let dateRow = PlanInfoRow().then {
+        $0.setLabel("약속 날짜")
+        $0.setText("2023. 07. 15")
+    }
+    
+    // 약속 시간
+    let timeRow = PlanInfoRow().then {
+        $0.setLabel("약속 시간")
+        $0.setText("10:00")
+    }
+    
+    // 참여자 label
+    let memberLabel = UILabel().then {
+        $0.text = "참여자"
+        $0.font = UIFont.body2Bold
+        $0.textColor = UIColor.textDisabled
+    }
+    
+    // 참여자 collection view
+    lazy var memberCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
+        $0.isScrollEnabled = false
+        $0.contentInset = UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
+        self.navigationController?.navigationBar.isHidden = false
+        navigationItem.title = "상세 페이지"
+        
+        // bottom sheet 버튼 추가
+        bottomSheetButton.addTarget(self, action: #selector(showBottomSheet), for: .touchUpInside)
+        let barButtonItem = UIBarButtonItem(customView: bottomSheetButton)
+        navigationItem.rightBarButtonItem = barButtonItem
+        
+        layoutConstraints()
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        memberCollectionView.delegate = self
+        memberCollectionView.dataSource = self
+        memberCollectionView.register(MemberCollectionViewCell.self, forCellWithReuseIdentifier: MemberCollectionViewCell.cellId)
+    }
+    
+    @objc private func showBottomSheet() {
+        let bottomSheetViewController = PlanEditDelBottomSheetViewController()
+        bottomSheetViewController.delegate = self
+        bottomSheetViewController.modalPresentationStyle = .overCurrentContext
+        bottomSheetViewController.modalTransitionStyle = .crossDissolve
+        present(bottomSheetViewController, animated: true, completion: nil)
+    }
+    
+    // 전체 layout
+    private func layoutConstraints() {
+        viewConstraints()
+        rowConstraints()
+        memberConstraints()
+    }
+    
+    // scrollview, contentsview layout
+    private func viewConstraints() {
+        view.addSubview(scrollview)
+        scrollview.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(0)
+        }
+        
+        scrollview.addSubview(contentsView)
+        contentsView.snp.makeConstraints { make in
+            make.edges.equalTo(scrollview.contentLayoutGuide)
+            make.width.equalTo(scrollview.frameLayoutGuide)
+            make.height.equalTo(1000)
+        }
+    }
+    
+    // row layout
+    private func rowConstraints() {
+        contentsView.addSubview(nameRow)
+        nameRow.snp.makeConstraints { make in
+            make.width.equalToSuperview().offset(-48)
+            make.height.equalTo(54)
+            make.top.equalTo(contentsView.snp.top).offset(12)
+            make.leading.equalTo(contentsView.snp.leading).offset(24)
+        }
+        
+        contentsView.addSubview(dateRow)
+        dateRow.snp.makeConstraints { make in
+            make.width.equalToSuperview().offset(-48)
+            make.height.equalTo(54)
+            make.top.equalTo(nameRow.snp.bottom).offset(26)
+            make.leading.equalTo(contentsView.snp.leading).offset(24)
+        }
+        
+        contentsView.addSubview(timeRow)
+        timeRow.snp.makeConstraints { make in
+            make.width.equalToSuperview().offset(-48)
+            make.height.equalTo(54)
+            make.top.equalTo(dateRow.snp.bottom).offset(26)
+            make.leading.equalTo(contentsView.snp.leading).offset(24)
+        }
+    }
+    
+    // 참여자 layout
+    private func memberConstraints() {
+        contentsView.addSubview(memberLabel)
+        memberLabel.snp.makeConstraints { make in
+            make.height.equalTo(14)
+            make.top.equalTo(timeRow.snp.bottom).offset(26)
+            make.leading.equalTo(contentsView.snp.leading).offset(24)
+        }
+        
+        contentsView.addSubview(memberCollectionView)
+        memberCollectionView.snp.makeConstraints { make in
+            make.width.equalToSuperview().offset(-48)
+            
+//            var memberRow = ceil(Double(members.count) / 2.0)
+            var memberRow = ceil(Double(3) / 2.0)
+            var height = (memberRow * 42) + ((memberRow - 1) * 10)
+            make.height.equalTo(height)
+            
+            make.top.equalTo(memberLabel.snp.bottom).offset(14)
+            make.leading.trailing.equalToSuperview().inset(24)
+        }
+    }
+}
+
+extension DecidedPlanInfoViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    // 각 셀을 클릭했을 때 이벤트 처리
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected cell at indexPath: \(indexPath)")
+    }
+    
+    // 셀 개수
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return members.count
+        return 3
+    }
+    
+    // 셀
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MemberCollectionViewCell.cellId, for: indexPath) as? MemberCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+//        cell.name.text = members[indexPath.item].name
+//        if let url = URL(string: members[indexPath.item].image) {
+//            cell.profileImage.kf.setImage(with: url, placeholder: UIImage(named: "defaultProfile"))
+//        }
+        cell.name.text = "참여참여"
+        cell.profileImage.image = UIImage(named: "defaultProfile")
+        
+        return cell
+    }
+    
+    // 셀 크기
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width
+        let halfWidth = (width - 10) / 2
+        return CGSize.init(width: halfWidth, height: 42)
+    }
+    
+    // 셀 사이의 위아래 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+}
+
+extension DecidedPlanInfoViewController: PlanInfoViewControllerDelegate {
+    func sendDataBack(data: String) {
+        if data == "pop" {
+            self.navigationController?.popViewController(animated: true)
+        } else {
+            let popUpVC = DeletePlanPopUpViewController()
+            popUpVC.planId = planId
+            popUpVC.delegate = self
+            popUpVC.modalPresentationStyle = .overCurrentContext
+            popUpVC.modalTransitionStyle = .crossDissolve
+            present(popUpVC, animated: true, completion: nil)
+        }
+    }
+}
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct ViewControllerPreview: PreviewProvider {
+    static var previews: some View {
+        DecidedPlanInfoViewController().showPreview(.iPhone14Pro)
+    }
+}
+#endif
